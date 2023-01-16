cb73d42d5932308b6e13a035c1b69ca16b0e5f8d
Desu <673464228@qq.com>
2023-01-16 17:34:50 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue'
 
-@Options({
+export default defineComponent({
+  name: 'App',
   components: {
-    HelloWorld
   }
 })
-export default class App extends Vue {}
 </script>
 
 <style lang="less">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,16 +33,14 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
+import { defineComponent } from 'vue'
 
-@Options({
+export default defineComponent({
+  name: 'HelloWorld',
   props: {
     msg: String
   }
 })
-export default class HelloWorld extends Vue {
-  msg!: string
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
