6700945f27027c5d09ad5940e006c4dea64862a7
Desu <673464228@qq.com>
2023-02-05 22:47:27 +0800

    config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import viog from '../packages'
+import viog from '../packages/index'
 // [Vue warn]: injected property "radio-group" is a ref and will be auto-unwrapped and no longer needs
 // `.value` in the next minor release. To opt-in to the new behavior now, set `app.config.unwrapInjectedRef = true` (this config is temporary and will not be needed in the future.)
 // at <ViRadio type="button" color="purple" dark=""  ... >
--- vue.config.js
+++ vue.config.js
@@ -3,19 +3,25 @@ const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // pages: {
-  //   index: {
-  //     entry: 'packages/index.ts'
-  //   }
+  pages: {
+    index: {
+      entry: 'packages/index.ts'
+    }
+  },
+  configureWebpack: {
+    output: {
+      libraryExport: 'default'
+    }
+  },
+  // chainWebpack: config => {
+  //   config.module.rule('js')
+  //   .include.add(path.resolve(__dirname, 'packages')).end()
+  //   .use('babel')
+  //   .loader('babel-loader')
+  //   .tap(options => {
+  //     // 暂时没哟
+  //     return options
+  //   })
   // },
-  chainWebpack: config => {
-    config.module.rule('ts')
-    .include.add(path.resolve(__dirname, 'packages')).end()
-    .use('babel')
-    .loader('babel-loader')
-    .tap(options => {
-      // 暂时没哟
-      return options
-    })
-  }
+  parallel: false
 })
