45d7377a3b64261f21c6943d7dcda07182747888
Desu <673464228@qq.com>
2023-04-03 21:49:12 +0800

    add suffix and postfix

---
+++ packages/input/src/input-fix.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="vi-input-fix">
+    <vi-select v-if="isArray" datas="data" v-model="modelValue"></vi-select>
+    <p v-else>{{data}}</p>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import type { SetupContext } from 'vue'
+
+export default defineComponent({
+  name: 'ViInput',
+  emits: ['change', 'input', 'update:modelValue'],
+  props: {
+    modelValue: {
+      default: ''
+    },
+    data: {
+      default: ''
+    }
+  },
+  setup (props: any, context: SetupContext) {
+    const isArray = ref(Array.isArray(props.data))
+    return {
+    }
+  }
+})
+</script>
+
+<style lang="less">
+  @import './css/fix.less';
+</style>
