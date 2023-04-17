dbf35b5a9a7c1a630e5de347847bd2eb6b4001cd
Desu <673464228@qq.com>
2023-04-17 21:03:13 +0800

    tag init

---
+++ packages/tag/index.ts
@@ -0,0 +1,8 @@
+import tag from './src/tag.vue'
+import { toInstall } from '../utils/component'
+
+export default toInstall({}, tag)
+
+export {
+  tag
+}
---
+++ packages/tag/src/tag.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="vi-tag">标签内容</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import type { SetupContext } from 'vue'
+
+export default defineComponent({
+  name: 'Vi',
+  setup(props: any, ctx: SetupContext) {
+  }
+})
+</script>
+
+<style lang="less">
+</style>
