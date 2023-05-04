c11055ec7472913b001aaef38dd673e54c8d05d5
Desu <673464228@qq.com>
2023-05-04 20:29:50 +0800

    dropdown

--- packages/dropdown/src/dropdown.vue
+++ packages/dropdown/src/dropdown.vue
@@ -1,15 +1,35 @@
 <template>
-    <span class="vi-dropdown">dropdown</span>
+    <span class="vi-dropdown" ref="select">
+      <div class="vi-dropdown-nav" @click="toSelect">dropdown</div>
+      <div class="vi-dropdown-content">
+        <transition name="vi-fade-in-out">
+          <scroll class="vi-dropdown-list" v-show="open">
+            1111111111111111111
+          </scroll>
+        </transition>
+      </div>
+    </span>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import type { SetupContext } from 'vue'
 
+import { scroll } from '../../scroll'
+
+import openState from '@/hooks/open-state'
+
 export default defineComponent({
   name: 'ViDropdown',
+  components: {
+    scroll
+  },
   setup (props: any, ctx: SetupContext) {
-    console.log('1')
+    const open = openState()
+
+    return {
+      ...open
+    }
   }
 })
 </script>
