a2777f574ebc131310f6600769a40560cacf7913
Desu <673464228@qq.com>
2023-05-05 22:02:16 +0800

    select

--- packages/select-normal/src/select.vue
+++ packages/select-normal/src/select.vue
@@ -1,41 +1,47 @@
 <template>
-  <span
+  <dropdown
   class="vi-select"
   :class="[
     `is-${size}`,
     `is-${type}`,
     {
       'is-dark': dark,
-      'be-open': open,
       'is-disabled': disabled
     }
-  ]"
-  ref="select">
-  <span class="vi-select-input"
-    @click="toSelect">
-        <span class="vi-select-choose">
-        </span>
-        <svg
-        class="vi-select-arrow"
-        viewBox="0 0 40 35"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
+  ]">
+    <template v-slot:default="{open}">
+      <span class="vi-select-input"
+      :class="[
+        {
+          'be-open': open
+        }
+      ]"
+      @click="toSelect">
+          <span class="vi-select-choose">
+          </span>
+          <svg
+          class="vi-select-arrow"
+          viewBox="0 0 40 35"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
             <path d="M17.4019 1.5C18.5566 -0.5 21.4434 -0.5 22.5981 1.5L39.0526 30C40.2073 32 38.7639 34.5 36.4545 34.5H3.54552C1.23612 34.5 -0.207259 32 0.947441 30L17.4019 1.5Z"/>
-        </svg>
-    </span>
-    <div class="vi-select-list">
-      <transition name="vi-select-box">
-        <div class="vi-select-list-container" v-if="open">
-          <slot></slot>
-        </div>
-      </transition>
-    </div>
-  </span>
+          </svg>
+      </span>
+    </template>
+    <template v-slot:content>
+      <scroll class="vi-select-box vi-select-box-normal">
+        <slot></slot>
+      </scroll>
+    </template>
+  </dropdown>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import type { SetupContext } from 'vue'
+
+import { dropdown } from '../../dropdown'
+import { scroll } from '../../scroll'
 
 import props from './props/select'
 
@@ -43,6 +49,10 @@ import openState from '@/hooks/open-state'
 
 export default defineComponent({
   name: 'ViSelectN',
+  components: {
+    dropdown,
+    scroll
+  },
   props,
   setup (props: any, ctx: SetupContext) {
     const open = openState()
