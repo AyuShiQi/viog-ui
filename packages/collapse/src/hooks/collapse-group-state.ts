0e4aba05babfd31f962efab09168216048d010d7
Desu <673464228@qq.com>
2023-05-11 18:52:52 +0800

    collapse

--- packages/collapse/src/hooks/collapse-group-state.ts
+++ packages/collapse/src/hooks/collapse-group-state.ts
@@ -1,15 +1,11 @@
 import { provide, ref } from 'vue'
+import IdDistributor from '../../../utils/communication/IdDistributor'
 
 export default function (props: any) {
   const accordionChoose = ref(0)
 
-  function idGetter () {
-    let id = 1
-    return () => id++
-  }
-
   if (props.accordion) {
-    provide('id', idGetter())
+    provide('id', IdDistributor())
 
     provide('change-choose', function (newChoose: number) {
       accordionChoose.value = newChoose
