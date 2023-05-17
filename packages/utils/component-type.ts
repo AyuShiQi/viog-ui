713aadce0841dc3b2158fea0da7d71b7b4d4953c
Desu <673464228@qq.com>
2023-05-17 22:07:32 +0800

    props type util

---
+++ packages/utils/component-type.ts
@@ -0,0 +1,21 @@
+export type PropsType<T> = {
+    [k in keyof T]: T[k] extends { type: any } ? (
+        T[k]['type'] extends Array<any> ? (
+            includeString<T[k]['type'][0]>
+        ) : normalType<T[k]['type']>
+    ) : normalType<T[k]> | undefined
+}
+
+type normalType<T> = T extends StringConstructor ? string :
+T extends BooleanConstructor ? boolean :
+T extends NumberConstructor ? number :
+T extends FunctionConstructor ? Function :
+T extends ObjectConstructor ? object : 
+T extends ArrayConstructor ? any[] : any
+
+type includeString<T> = StringConstructor extends T ? string | includeBoolean<T> : includeBoolean<T>
+type includeBoolean<T> = BooleanConstructor extends T ? boolean | includeNumber<T> : includeNumber<T>
+type includeNumber<T> = NumberConstructor extends T ? number | includeFunction<T> : includeFunction<T>
+type includeFunction<T> = FunctionConstructor extends T ? Function | includeObject<T> : includeObject<T>
+type includeObject<T> = ObjectConstructor extends T ? object | includeArray<T> : includeArray<T>
+type includeArray<T> = ArrayConstructor extends T ? any[] : never
