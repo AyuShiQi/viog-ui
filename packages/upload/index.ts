809a6a551e640eaec6813c83a999a52cfec8f9b3
Desu <673464228@qq.com>
2023-06-12 13:36:57 +0800

    fix drpdown and upload

--- packages/upload/index.ts
+++ packages/upload/index.ts
@@ -2,6 +2,7 @@ import upload from './src/upload.vue'
 import uploadChoose from './src/upload-choose.vue'
 import uploadList from './src/upload-list.vue'
 import { toInstall } from '../utils/component'
+import './src/img'
 
 export default toInstall({}, upload, uploadChoose, uploadList)
 export {
