ae9432acfda17f04b621fa1b34bcc9aa14f250cf
Desu <673464228@qq.com>
2023-07-01 16:05:10 +0800

    prev：优化固定栏

--- packages/table-editor/src/table-editor.vue
+++ packages/table-editor/src/table-editor.vue
@@ -1,93 +1,92 @@
 <template>
-  <scroll
-  class="vi-table-editor"
-  ref="table">
-    <div class="vi-table-editor__view" draggable="false">
-      <div class="vi-table-editor__fixed-header"
-      :style="{
-        top: `${tableTop}px`,
-        left: `${tableLeft}px`
-      }"></div>
-      <!-- 侧边栏 -->
-      <div class="vi-table-editor__side-header"
-      :style="{
-        left: `${tableLeft}px`
+  <div class="vi-table-editor">
+    <!-- 固定头 -->
+    <div class="vi-table-editor__fixed-header"></div>
+    <!-- 侧边栏 -->
+    <div class="vi-table-editor__side-header"
+    :style="{
+      transform: `translateY(-${tableTop}px)`
+    }">
+      <span class="vi-table-editor__side-header__th"
+      v-for="(item, i) in sideList"
+      :key="item"
+      @mousedown="handleSiderClick($event, i)"
+      @contextmenu="handleOpenContextMenu($event, 'row')"
+      :class="{
+        'is-entire': entireTarget[0] === i || chooseTarget[0] === i
       }">
-        <span class="vi-table-editor__side-header__th"
-        v-for="(item, i) in sideList"
-        :key="item"
-        @mousedown="handleSiderClick($event, i)"
-        @contextmenu="handleOpenContextMenu($event, 'row')"
-        :class="{
-          'is-entire': entireTarget[0] === i || chooseTarget[0] === i
-        }">
-          {{ item }}
-        </span>
-      </div>
-      <!-- 头部 -->
-      <div class="vi-table-editor__header"
-      :style="{
-        top: `${tableTop}px`
-      }">
-        <span
-        class="vi-table-editor__header__th"
-        v-for="(item, j) in headerList"
-        :key="item"
-        @mousedown="handleHeaderClick($event, j)"
-        @contextmenu="handleOpenContextMenu($event, 'col')"
-        :class="{
-          'is-entire': entireTarget[1] === j || chooseTarget[1] === j
-        }">
-          {{ item }}
-        </span>
-      </div>
-      <!-- body部分 -->
-      <div class="vi-table-editor__body" draggable="false" ref="tableBody">
-        <div class="vi-table-editor__body__tr" v-for="(arr, i) in value" :key="arr">
-          <tableEditorTd
-          v-for="(item, j) in arr"
-          :key="`${item}$${i}$${j}`"
-          :value="item" :row="Number(i)"
-          :col="j"
-          @contextmenu="handleOpenContextMenu"></tableEditorTd>
-        </div>
-      </div>
-      <!-- 格式框 -->
-      <div
-      v-show="needChange"
-      class="vi-table-editor__change-box"
-      :style="{
-        top: changeTop,
-        left: changeLeft,
-        width: changeWidth,
-        height: changeHeight
-      }"></div>
-      <!-- 选中框 -->
-      <div
-      v-show="pickBoxShow"
-      class="vi-table-editor__pick-box"
-      :style="{
-        top: pickTop,
-        left: pickLeft,
-        width: pickWidth,
-        height: pickHeight
-      }">
-        <div class="vi-table-editor__pick-pointer"
-        @mousedown="handlePointerMouseDown"
-        :style="{
-          'pointer-events': needChange ? 'none' : 'auto'
-        }"
-        ></div>
-      </div>
-      <!-- 操作列表 -->
-      <teleport to='body'>
-        <tableEditorOptionList
-        v-show="open"
-        ref="openDOM">
-        </tableEditorOptionList>
-      </teleport>
+        {{ item }}
+      </span>
     </div>
-  </scroll>
+    <!-- 头部 -->
+    <div class="vi-table-editor__header"
+    :style="{
+      transform: `translateX(-${tableLeft}px)`
+    }">
+      <span
+      class="vi-table-editor__header__th"
+      v-for="(item, j) in headerList"
+      :key="item"
+      @mousedown="handleHeaderClick($event, j)"
+      @contextmenu="handleOpenContextMenu($event, 'col')"
+      :class="{
+        'is-entire': entireTarget[1] === j || chooseTarget[1] === j
+      }">
+        {{ item }}
+      </span>
+    </div>
+    <scroll
+    class="vi-table-editor__scroll"
+    ref="table">
+      <div class="vi-table-editor__view" draggable="false">
+        <!-- body部分 -->
+        <div class="vi-table-editor__body" draggable="false" ref="tableBody">
+          <div class="vi-table-editor__body__tr" v-for="(arr, i) in value" :key="arr">
+            <tableEditorTd
+            v-for="(item, j) in arr"
+            :key="`${item}$${i}$${j}`"
+            :value="item" :row="Number(i)"
+            :col="j"
+            @contextmenu="handleOpenContextMenu"></tableEditorTd>
+          </div>
+        </div>
+        <!-- 格式框 -->
+        <div
+        v-show="needChange"
+        class="vi-table-editor__change-box"
+        :style="{
+          top: changeTop,
+          left: changeLeft,
+          width: changeWidth,
+          height: changeHeight
+        }"></div>
+        <!-- 选中框 -->
+        <div
+        v-show="pickBoxShow"
+        class="vi-table-editor__pick-box"
+        :style="{
+          top: pickTop,
+          left: pickLeft,
+          width: pickWidth,
+          height: pickHeight
+        }">
+          <div class="vi-table-editor__pick-pointer"
+          @mousedown="handlePointerMouseDown"
+          :style="{
+            'pointer-events': needChange ? 'none' : 'auto'
+          }"
+          ></div>
+        </div>
+        <!-- 操作列表 -->
+        <teleport to='body'>
+          <tableEditorOptionList
+          v-show="open"
+          ref="openDOM">
+          </tableEditorOptionList>
+        </teleport>
+      </div>
+    </scroll>
+  </div>
 </template>
 
 <script lang="ts">
